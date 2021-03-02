b012957a77393995e601d4bb421547bac02c0631
michael <michaeljchap@gmail.com>
2021-03-02 00:36:42 -0500

    Started adding templates

--- app/models.py
+++ app/models.py
@@ -13,28 +13,51 @@ class Channel(db.Model):
 	range_eu = db.Column(db.Integer) # 0 = degC, 1 = Hz, 2 = lbf, etc...
 	tolerance = db.Column(db.Float(8))
 	tolerance_eu = db.Column(db.Integer) # 0 = %FS, 1 = %RDG, 2 = EU, etc...
+	test_min_range = db.Column(db.Float(16))
+	test_max_range = db.Column(db.Float(16))
+	test_eu = db.Column(db.Integer) # 0 = degC, 1 = Hz, 2 = lbf, etc...
 	test_points = db.relationship('TestPoint', backref='channel', lazy='dynamic')
+	cal_eq_id_1 = db.Column(db.Integer)
+	cal_eq_id_1_due_date = db.Column(db.DateTime, default=datetime.utcnow)
 	
 	def __repr__(self):
 		return '<Channel {}>'.format(self.name)
+
+	def test_3p(self):
+		# Return default points for a 3 point test based on mix/max
+
+	def test_5p(self):
+		# Return default points for a 5 point test based on min/max
 	
 	
 class TestPoint(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	channel_id = db.Column(db.Integer, db.ForeignKey('channel.id'))
-	date_performed = db.Column(db.DateTime, default=datetime.utcnow)
-	min = db.Column(db.Float(16))
-	max = db.Column(db.Float(16))
-	eu = db.Column(db.Integer) # 0 = ohms, 1 = Hz, 2 = mV/V, etc...
-	error = db.Column(db.Float(12))
-	error_eu = db.Column(db.Integer, db.ForeignKey('channel.range_eu'))
+	input_val = db.Column(db.Float(16))
+	measured_val = db.Column(db.Float(16))
+	nominal_val = db.Column(db.Float(16))
 	pf = db.Column(db.Integer) # 0 = Fail, 1 = Pass
+	date_performed = db.Column(db.DateTime, default=datetime.utcnow)
 	notes = db.Column(db.String(128))
-	cal_eq_id_1 = db.Column(db.Integer)
-	cal_eq_id_1_due_date = db.Column(db.DateTime)
-	cal_eq_id_2 = db.Column(db.Integer)
-	cal_eq_id_2_due_date = db.Column(db.DateTime)
 	
 	def __repr__(self):
 		return '<TestPoint {} for Channel id {}>'.format(self.id, self.channel_id)
+
+	def tolerance(self):
+		return Channel.query.filter_by(id=self.channel_id).first().tolerance
+
+	# Need to handle EU or %
+	def low_limit(self, tol_eu):
+		if tol_eu == 'eu':
+			limit = nominal_val - tolerance
+		elif tol_eu == 'fs':
+			limit = 
+		return nominal_val - tolerance
+
+	# Need to handle EU or %
+	def high_limit(self):
+		return nominal_val + tolerance
 	
+	def calc_error(self):
+
+	def calc_pf(self):
