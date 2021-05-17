a7d3094bbe2cdca91fb4cafebb83f7859a2311b6
michael <michaeljchap@gmail.com>
2021-05-17 07:54:14 -0400

    Fixed buttongroup

--- todo_list.py
+++ todo_list.py
@@ -4,7 +4,6 @@
  - Add buttons to digitally sign on client vs. user accounts to accept testpoints, make buttons disabled until all fields filled in
  - Add calibration equipment records to each test point, including the cal due dates
  - Add a chart to the testpoint view
- - Add ability to add a new testpoint in the channel list view
  - Add ability to change units on a testpoint in the channel list view
  - Add ability to convert between similar unit types (psig -> psia, degC to degF, Ohms -> degC (Pt100)) in new channel form or channel list view
  - Add ability to import MCL data to populate the ICT for a project
@@ -13,5 +12,8 @@
  - Add dividers to the testpoint progress bars
  - Add colours into the min/max of the measured column if error is exceeded, maybe make text too
  - Move all the if statements in the _testpoint.html file into the routes file to pre-prep the form fields
- 
+ - Add an info button on a channel to peek at the remaining undisplayed data from the creation of the channel
+ - Add badges into the channel list header to show number of pass/fail/posts
+ - Add an edit button in the channel view to make visible new buttons to add/remove testpoints in a row
+
  """
